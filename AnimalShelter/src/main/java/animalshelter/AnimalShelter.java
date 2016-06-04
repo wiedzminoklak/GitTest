43350ee7f120233e8dfbe7968ed8bef8501632f5
wiedzminoklak <wiedzminoklak@outlook.com>
2016-06-04 11:10:30 +0200

    Another commit

--- AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
+++ AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
@@ -6,6 +6,7 @@ public class AnimalShelter {
 		System.out.println("Animal Shelter");
 		System.out.println("Second commit");
 		System.out.println("Third commit");
+		System.out.println("Another commit");
 	}
 
 }
