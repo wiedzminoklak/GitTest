a65507cd2159f7014786685f71119e4865ca3e9e
wiedzminoklak <wiedzminoklak@outlook.com>
2016-06-04 11:25:17 +0200

    develop commit 1

--- AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
+++ AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
@@ -4,12 +4,20 @@ public class AnimalShelter {
 
 	public static void main(String[] args) {
 		System.out.println("Animal Shelter");
+		System.out.println("Second commit");
 		for(int i = 0; i < 6; ++i) {
-			System.out.println("Second commit");
 			System.out.println("hue hue hue");
 		}	
 		System.out.println("Third commit");
 		System.out.println("Another commit");
+		
+		String password = "tajne";
+		
+		if(password.equals("tajne")) {
+			System.out.println("Welcome unknow");
+		} else {
+			System.out.println("Access denied!");
+		}
 	}
 
 }
