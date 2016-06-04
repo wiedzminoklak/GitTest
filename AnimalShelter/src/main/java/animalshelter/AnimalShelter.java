df97591e5dc2023c48aafd6220b9ba009ecab409
wiedzminoklak <wiedzminoklak@outlook.com>
2016-06-04 11:11:43 +0200

    For commit

--- AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
+++ AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
@@ -4,7 +4,9 @@ public class AnimalShelter {
 
 	public static void main(String[] args) {
 		System.out.println("Animal Shelter");
-		System.out.println("Second commit");
+		for(int i = 0; i < 3; ++i) {
+			System.out.println("Second commit");
+		}	
 		System.out.println("Third commit");
 		System.out.println("Another commit");
 	}
