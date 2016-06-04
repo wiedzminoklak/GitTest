8a7413f84104c417217c50c112fc74a1e76d242e
wiedzminoklak <wiedzminoklak@outlook.com>
2016-06-04 11:05:56 +0200

    Animal Shelter Third commit amend

--- AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
+++ AnimalShelter/src/main/java/animalshelter/AnimalShelter.java
@@ -5,6 +5,7 @@ public class AnimalShelter {
 	public static void main(String[] args) {
 		System.out.println("Animal Shelter");
 		System.out.println("Second commit");
+		System.out.println("Third commit");
 	}
 
 }
